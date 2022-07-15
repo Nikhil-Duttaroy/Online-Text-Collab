bdd91c797f1af7126064758089c727bb13244eab
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2022-07-15 20:43:43 +0530

    functionality to join and create room added

--- src/Pages/HomePage/HomePage.component.jsx
+++ src/Pages/HomePage/HomePage.component.jsx
@@ -1,24 +1,65 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.svg";
 import "./HomePage.styles.css";
+import { useNavigate } from "react-router-dom";
 
 import { v4 as uuid } from "uuid";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const HomePage = () => {
-  const [roomId, setRoomId] = useState('');
-  const [userName, setUserName] = useState('');
-  
-  const notify = () =>toast.success("New Room Created");
-  
+  //States
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  //hooks
+  const navigate = useNavigate();
+
+  //functions
+  const notifySuccess = () =>
+    toast.success("New Room Created", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const notifyError = () =>
+    toast.error("Room Id and UserName Required", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+  const handleInputEnter = (e) => {
+    if (e.key === "Enter") {
+      joinRoom();
+    }
+  };
+
   const createNewRoom = (e) => {
     const id = uuid();
     setRoomId(id);
-    notify();
+    notifySuccess();
   };
 
-
+  const joinRoom = (e) => {
+    if (!roomId || !userName) {
+      notifyError();
+      return;
+    }
+    navigate(`/editor/${roomId}`, {
+      state: {
+        userName,
+      },
+    });
+  };
 
   return (
     <div className='homePageWrapper'>
@@ -31,6 +72,7 @@ const HomePage = () => {
             className='inputBox'
             placeholder='Room Id'
             onChange={(e) => setRoomId(e.target.value)}
+            onKeyUp={handleInputEnter}
             value={roomId}
           />
           <input
@@ -38,25 +80,18 @@ const HomePage = () => {
             className='inputBox'
             placeholder='User Name'
             onChange={(e) => setUserName(e.target.value)}
+            onKeyUp={handleInputEnter}
             value={userName}
           />
-          <button className='btn joinBtn'>Join Room</button>
+          <button className='btn joinBtn' onClick={joinRoom}>
+            Join Room
+          </button>
           <button className='btn createBtn' onClick={createNewRoom}>
             Create Room
           </button>
         </div>
       </div>
-      <ToastContainer
-        position='top-right'
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <ToastContainer />
     </div>
   );
 };
