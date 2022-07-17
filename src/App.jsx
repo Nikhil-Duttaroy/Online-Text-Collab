17f807816d674571b3f1ee7961a9433c7883198e
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2022-07-17 19:20:34 +0530

    toastContainer added for universal use

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import './App.css'
 import HomePage from './Pages/HomePage/HomePage.component';
 import EditorPage from './Pages/EditorPage/EditorPage.component';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -9,14 +10,13 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage/>}></Route>
-          <Route path="/editor/:roomId" element={<EditorPage/>}></Route>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/editor/:roomId' element={<EditorPage />}></Route>
         </Routes>
-      
       </Router>
-    
+      <ToastContainer />
     </>
-  )
+  );
 }
 
 export default App
